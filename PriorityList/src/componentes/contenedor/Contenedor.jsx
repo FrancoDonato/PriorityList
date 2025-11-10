be1a7e170e14f36933ad14975531f7008a948452
unknown <francodonato33@gmail.com>
2025-11-10 14:44:34 -0300

    se agrega navbar y colores de backgorund

--- PriorityList/src/componentes/contenedor/Contenedor.jsx
+++ PriorityList/src/componentes/contenedor/Contenedor.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
 
 const Contenedor = () => {
   return (
-    <div>Contenedor</div>
-  )
+    <Navbar></Navbar>
+    )
 }
 
 export default Contenedor
